2471bcf8f45f3568567512c0093a97301af047bd
MacStudent <macstudent@20x-imacxx-m-473.local>
2018-08-08 15:11:31 -0400

    Initial Commit

---
+++ Electricity_Bill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Electricity_Bill
+//
+//  Created by MacStudent on 2018-08-08.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
